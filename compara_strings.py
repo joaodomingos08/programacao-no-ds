5429a5a3c5b0b9d11ed62697f667a08d7066f737
joaodomingos08 <domingos.costa.joao@escola.pr.gov.br>
2025-04-10 09:24:24 -0300

    Create compara_strings.py

---
+++ compara_strings.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário que insira duas frases
+frase1 = input("Digite a 1ª frase: ")
+frase2 = input("Digite a 2ª frase: ")
+
+# Verifica comparando se as frases são idênticas
+if frase1 == frase2:
+    print("As frases são idênticas.")
+else:
+    print("As frases são diferentes.")
