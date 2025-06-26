d85d7681cdc17511926e649683e3d433ae31384a
joaodomingos08 <domingos.costa.joao@escola.pr.gov.br>
2025-06-26 10:09:17 -0300

    Create busca-livros.py

---
+++ busca-livros.py
@@ -0,0 +1,11 @@
+# Solicita entrada de dado
+nome_usuario = input("Digite seu nome em letras minúsculas: ") 
+# Converte string em maiúsculas
+nome_formatado = nome_usuario.upper()
+# Mostra o resultado da conversão com upper()
+print(f"Seu nome formatado: {nome_formatado}")  # Solicita entrada de dado
+nome_usuario = input("Digite seu nome em letras minúsculas: ") 
+# Converte string em maiúsculas
+nome_formatado = nome_usuario.upper()
+# Mostra o resultado da conversão com upper()
+print(f"Seu nome formatado: {nome_formatado}")
